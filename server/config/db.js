75854533e393d46b6751677805d4702ca69d8fd7
edwintsanghm <edwintsanghm@gmail.com>
2022-03-25 13:58:06 -0400

    remove auth for UI dev

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1:27017/centennial_courses",
+    // "URI": "mongodb://127.0.0.1:27017/centennial_courses",
     // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
-    //"URI": "mongodb+srv://groupadmin:groupadmin1234@cluster0.d8pka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://groupadmin:groupadmin1234@cluster0.d8pka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "Secret": 'group6'
 }
