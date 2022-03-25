776ad3c26c0e77f102c2ab6d5fa44fbb74324b0a
edwintsanghm <edwintsanghm@gmail.com>
2022-03-25 13:47:00 -0400

    remove auth for UI dev

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -3,23 +3,40 @@ let router = express.Router();
 var auth = require('./auth');
 let surveyController = require('../controllers/survey');
 
+// TODO: comment out auth for development
 /* GET Route for the Survey List page - READ Operation */
-router.get('/',auth.required,surveyController.displaySurveyList);
+// router.get('/',auth.required,surveyController.displaySurveyList);
 
+// /* POST Route for processing the Add page - CREATE Operation */
+// router.post('/add',auth.required, surveyController.processAddPage);
+
+// /* GET Route for displaying the Edit page - UPDATE Operation */
+// router.post('/edit/:id',auth.required,surveyController.processEditPage);
+
+// /* GET Route for displaying the Edit page - UPDATE Operation */
+// router.get('/:id',auth.required,surveyController.displaySpecificSurvey);
+
+// /* GET to perform  Deletion - DELETE Operation */
+// router.get('/delete/:id',auth.required, surveyController.performDelete);
+
+// /** GET Route for responses summary */
+// router.get('/summary/:id',auth.required,surveyController.displaySummary);
+
+router.get('/',surveyController.displaySurveyList);
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add',auth.required, surveyController.processAddPage);
+router.post('/add', surveyController.processAddPage);
 
 /* GET Route for displaying the Edit page - UPDATE Operation */
-router.post('/edit/:id',auth.required,surveyController.processEditPage);
+router.post('/edit/:id',surveyController.processEditPage);
 
 /* GET Route for displaying the Edit page - UPDATE Operation */
-router.get('/:id',auth.required,surveyController.displaySpecificSurvey);
+router.get('/:id',surveyController.displaySpecificSurvey);
 
 /* GET to perform  Deletion - DELETE Operation */
-router.get('/delete/:id',auth.required, surveyController.performDelete);
+router.get('/delete/:id', surveyController.performDelete);
 
 /** GET Route for responses summary */
-router.get('/summary/:id',auth.required,surveyController.displaySummary);
+router.get('/summary/:id',surveyController.displaySummary);
 
 module.exports = router;
