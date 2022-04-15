0f03bbcc9c3addcbc952692d3981fab57f353581
Jason Li <li.jason.tl@gmail.com>
2022-04-14 22:21:37 -0400

    add export button

--- client/src/app/survey-stat/survey-stat.component.ts
+++ client/src/app/survey-stat/survey-stat.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SurveyService } from '../survey.service';
 import { ActivatedRoute,Router,ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs';
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-survey-stat',
   templateUrl: './survey-stat.component.html',
@@ -35,6 +37,54 @@ export class SurveyStatComponent implements OnInit {
     console.log(stat)
     this.isShowChart = stat?true:false;
     this.stat = stat;
+  }
+
+  exportexcel(): void
+  {
+    /* pass here the table id */
+
+    let arr :any= [];  
+    const mydata = this.dataSource.summary;
+
+    mydata.forEach((currentValue: { title: any; stat:any}, index: any) => {
+      if(!!currentValue.stat){
+
+      let isFirst = true;
+      Object.keys(currentValue.stat).map(function(key){  
+        
+
+        if(isFirst){
+          arr.push({'Question':currentValue.title,'Answer':key, 'Count':currentValue.stat[key]})  
+        } else{
+          arr.push({'Answer':key, 'Count':currentValue.stat[key]}) 
+        } 
+
+        isFirst = false;
+          return arr;  
+      });  
+    }
+    });
+
+    
+    /*
+    console.log(mydata)
+    Object.keys(mydata).map(function(key){  
+      console.log(key);
+      console.log(mydata[key]);
+        arr.push({'ans':key,
+        'cnt':mydata[key]})   
+        return arr;  
+    });  
+    */
+    const ws: XLSX.WorkSheet =XLSX.utils.json_to_sheet(  arr);
+    
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, 'summary.xlsx');
+ 
   }
 
 }
